08115e624e199348093d42ad166084da31c03767
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-24 03:11:59 +0700

    Update JoinListener.java

--- src/main/java/me/vennlmao/arisadder/JoinListener.java
+++ src/main/java/me/vennlmao/arisadder/JoinListener.java
@@ -1,15 +1,16 @@
 package me.vennlmao.arisadder;
 
-import org.bukkit.event.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        String u = ArisAdder.getInstance().getConfig().getString("resource-pack.url");
-        String h = ArisAdder.getInstance().getConfig().getString("resource-pack.hash");
-        if (ArisAdder.getInstance().getConfig().getBoolean("resource-pack.enabled") && !u.isEmpty()) {
-            e.getPlayer().setResourcePack(u, h);
+        String url = ArisAdder.getInstance().getConfig().getString("resource-pack.url", "");
+        String hash = ArisAdder.getInstance().getConfig().getString("resource-pack.hash", "");
+        if (!url.isEmpty()) {
+            e.getPlayer().setResourcePack(url, hash.toLowerCase());
         }
     }
 }
